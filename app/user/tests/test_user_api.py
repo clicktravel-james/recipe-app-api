1998fa9dc5a63a49e02839ce894d3cd0db88e381
James Butherway <james.butherway@clicktravel.com>
2021-08-20 15:40:04 +0100

    Added a new authenticate token endpoint for API auth

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,136 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users public API"""
+
+    def setUp(self):
+        self.clinet = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating a new valid user"""
+        """Given """
+        payload = {
+            'email': 'jimmyjenkins@borderlands.com',
+            'password': 'Password1',
+            'name': 'Jimmy Jenkins',
+        }
+
+        """When"""
+        res = self.clinet.post(CREATE_USER_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating a user that already exists fails"""
+        """Given """
+        payload = {
+            'email': 'jimmyjenkins@borderlands.com',
+            'password': 'Password1',
+            'name': 'Jimmy Jenkins',
+        }
+        create_user(**payload)
+
+        """When"""
+        res = self.clinet.post(CREATE_USER_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that you cannot create a user with a very short password"""
+        """Given """
+        payload = {
+            'email': 'jimmyjenkins@borderlands.com',
+            'password': 'god',
+            'name': 'Jimmy Jenkins',
+        }
+
+        """When"""
+        res = self.clinet.post(CREATE_USER_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        does_user_exist = get_user_model().objects.filter(
+            email='jimmyjenkins@borderlands.com'
+        ).exists()
+        self.assertFalse(does_user_exist)
+
+    def test_create_token_for_user(self):
+        """Test for generating a token for a valid user"""
+        """Given """
+        payload = {
+            'email': 'jimmyjenkins@borderlands.com',
+            'password': 'Password1',
+        }
+        create_user(**payload)
+
+        """When"""
+        res = self.clinet.post(TOKEN_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('token', res.data)
+
+    def test_create_token_for_invlaid_credentails(self):
+        """Test that we get an error response for token if password wrong"""
+        """Given """
+        user_email = 'jimmyjenkins@borderlands.com'
+        create_user(email=user_email, password='Password1')
+        payload = {
+            'email': user_email,
+            'password': 'wrong',
+        }
+
+        """When"""
+        res = self.clinet.post(TOKEN_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+    def test_create_token_for_mising_user(self):
+        """Test for error response requesting token with no user"""
+        """Given """
+        payload = {
+            'email': 'jimmyjenkins@borderlands.com',
+            'password': 'Password1',
+        }
+
+        """When"""
+        res = self.clinet.post(TOKEN_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+    def test_create_token_for_missing_request_fields(self):
+        """Test that we get an error response for token if no password"""
+        """Given """
+        payload = {
+            'email': 'jimmyjenkins@borderlands.com',
+            'password': '',
+        }
+
+        """When"""
+        res = self.clinet.post(TOKEN_URL, payload)
+
+        """Then"""
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
